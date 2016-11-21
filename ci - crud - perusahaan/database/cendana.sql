755fe7c06311dc8d10753deda97546ba84806197
rizalferdian <rizalferdian95@gmail.com>
2016-11-21 15:57:48 +0700

    database

--- ci - crud - perusahaan/database/cendana.sql
+++ ci - crud - perusahaan/database/cendana.sql
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `login` (
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `username`, `password`, `img`) VALUES
+INSERT INTO `login` (`id`, `username`, `password`, `
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'me.jpg');
 
 -- --------------------------------------------------------
