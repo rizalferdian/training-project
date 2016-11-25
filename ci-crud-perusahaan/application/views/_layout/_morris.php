d04d5cd5658847022e4e0668067396497b304a58
rizalferdian <rizalferdian95@gmail.com>
2016-11-26 06:18:14 +0700

    delete

--- ci-crud-perusahaan/application/views/_layout/_morris.php
+++
@@ -1,4 +0,0 @@
-<!-- Morris Charts JavaScript -->
-<script src="<?php echo base_url('assets'); ?>/vendor/raphael/raphael.min.js"></script>
-<script src="<?php echo base_url('assets'); ?>/vendor/morrisjs/morris.min.js"></script>
-<script src="<?php echo base_url('chart'); ?>"></script>
